7f7ac2fbd67056a5ccc1ee1698610985ee8f8777
swiffo <svegstrup@gmail.com>
2016-08-27 10:58:01 +0100

    Add human player

--- connectfourplayers.py
+++ connectfourplayers.py
@@ -13,7 +13,10 @@ A player class must implement the following methods:
 Classes:
     RandomPlayer
 """
+
 import random
+import connectfour
+import connectfourgame
 
 class RandomPlayer:
     """A player making a random legal move each turn."""
@@ -39,3 +42,45 @@ class RandomPlayer:
             reward: The reward for the last move
         """
         pass
+
+class HumanPlayer:
+    """A player taking input from user"""
+
+    def propose_move(self, game_grid, player_colour):
+        """Propose next move.
+
+        Args:
+            game_grid: ConnectFour() game_grid with current position
+            player_colour: The colour for this player
+
+        Returns:
+            The column to which to add a disc (int)
+        """
+        if player_colour == connectfour.WHITE:
+            player_colour_string = 'white'
+        elif player_colour == connectfour.RED:
+            player_colour_string = 'red'
+        else:
+            raise ValueError('Unhandled colour')
+
+        print('You a playing {}'.format(player_colour_string))
+        print(game_grid)
+        print('Choose column (0 (left) to {} (right):'.format(connectfour.COLUMNS-1))
+        while True:
+            try:
+                user_choice = int(input())
+            except ValueError:
+                continue
+            break
+
+        return user_choice
+
+    def receive_reward(self, reward):
+        """Record the reward for the last move.
+
+        Args:
+            reward: The reward for the last move
+        """
+        pass
+
+
