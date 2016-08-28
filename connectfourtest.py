a74a82c37fae7cbf7a51dc3cab86ce22126265ef
swiffo <svegstrup@gmail.com>
2016-08-28 09:30:26 +0100

    Add script for executing all tests for Connect Four

---
+++ connectfourtest.py
@@ -0,0 +1,12 @@
+"""Full test suite for Connect Four"""
+
+import connectfour
+import connectfourgame
+
+def full_test():
+    """Run all tests for Connect Four"""
+    connectfour.test()
+    connectfourgame.test()
+
+if __name__ == '__main__':
+    full_test()
