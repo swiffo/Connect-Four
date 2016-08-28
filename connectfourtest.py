059bf1de8e636df854f5004f14c08ed302ba9809
swiffo <svegstrup@gmail.com>
2016-08-28 19:32:31 +0100

    Add test of connectfourplayers.py to the tests.

--- connectfourtest.py
+++ connectfourtest.py
@@ -2,11 +2,13 @@
 
 import connectfour
 import connectfourgame
+import connectfourplayers
 
 def full_test():
     """Run all tests for Connect Four"""
     connectfour.test()
     connectfourgame.test()
+    connectfourplayers.test()
 
 if __name__ == '__main__':
     full_test()
